e5a365b6a0e81d61b609430f5508bd6330686687
Dominic Magnifico <dominic@zenman.com>
2014-06-10 10:19:24 -0600

    Initial Commit

---
+++ custom-functions.php
@@ -0,0 +1,28 @@
+<?php
+// Facebook Open Graph
+add_action('wp_head', 'add_fb_open_graph_tags');
+function add_fb_open_graph_tags() {
+    if (is_single()) {
+        global $post;
+        if(get_the_post_thumbnail($post->ID, 'thumbnail')) {
+            $thumbnail_id = get_post_thumbnail_id($post->ID);
+            $thumbnail_object = get_post($thumbnail_id);
+            $image = $thumbnail_object->guid;
+        } else {    
+            $image = ''; // Change this to the URL of the logo you want beside your links shown on Facebook
+        }
+        //$description = get_bloginfo('description');
+        $description = my_excerpt( $post->post_content, $post->post_excerpt );
+        $description = strip_tags($description);
+        $description = str_replace("\"", "'", $description);
+?>
+<!-- Facebook -->
+<meta property="og:title" content="<?php the_title(); ?>" />
+<meta property="og:type" content="article" />
+<meta property="og:image" content="<?php echo $image; ?>" />
+<meta property="og:url" content="<?php the_permalink(); ?>" />
+<meta property="og:description" content="<?php echo $description ?>" />
+<meta property="og:site_name" content="<?php echo get_bloginfo('name'); ?>" />
+
+<?php   }
+}
---
+++ page-social.php
@@ -0,0 +1,43 @@
+<?php 
+        $excerpt = strip_tags(get_the_excerpt());
+        $post_thumbnail_id = get_post_thumbnail_id($post->ID);
+        $post_thumbnail_url = wp_get_attachment_url( $post_thumbnail_id );
+
+    ?>
+<div class="social__buttons">
+    <ul>
+        <li class="facebook">
+            <!-- Replace with your URL. For best results, make sure you page has the proper FB Open Graph tags in header: 
+            https://developers.facebook.com/docs/opengraph/howtos/maximizing-distribution-media-content/ -->
+            <a href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>" class="popup">
+                <span class="text">facebook</span>
+            </a>
+        </li>
+        <li class="linkedin">
+            <a href="http://www.linkedin.com/shareArticle?mini=true&amp;url=<?php the_permalink(); ?>&amp;title=<?php the_title(); ?>&amp;summary=<?php echo $excerpt; ?>" class="popup">
+                <span class="text">linkedin</span>
+            </a>
+        </li>
+        <li class="twitter">
+            <a href="http://twitter.com/home?status=<?php the_title(); ?> <?php the_permalink(); ?> via @ZenmanDenver" class="popup">
+                <span class="text">twitter</span>
+            </a>
+        </li>
+       <li class="reddit">
+            <a href="http://www.reddit.com/submit?url=<?php the_permalink(); ?>">
+                <span class="text">reddit</span>
+            </a>
+        </li>
+        <li class="googleplus">
+            <!-- Replace href with your meta and URL information.  -->
+            <a href="https://plus.google.com/share?url=<?php the_title(); the_permalink(); ?>" class="popup">
+                <span class="text">google+</span>
+            </a>
+        </li>
+        <li class="pinterest">
+            <a href="http://pinterest.com/pin/create/button/?url=<?php the_permalink(); ?>&amp;media=<?php echo $post_thumbnail_url; ?>&amp;description=<?php echo $excerpt; ?>">
+                <span class="text">pinterest</span>
+            </a>
+        </li>
+    </ul>
+</div>
